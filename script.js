c8b911e7c4b04cc47e8a7169f6903282f7fcfe90
sainagendra vutla <136782605+Phinehas1-1@users.noreply.github.com>
2024-11-20 22:54:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        id: 1,
+        title: "Book",
+        image: "phone.jpeg",
+        price: 499.99
+    },
+    {
+        id: 2,
+        title: "book",
+        image: "book.jpg",
+        price: 19.99
+    }
+];
+
+const container = document.getElementById('container');
+
+data.forEach(product => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const image = document.createElement('img');
+    image.src = product.image;
+    card.appendChild(image);
+
+    const title = document.createElement('h3');
+    title.textContent = product.title;
+    card.appendChild(title);
+
+    const price = document.createElement('p');
+    price.textContent = `$${product.price}`;
+    card.appendChild(price);
+
+    container.appendChild(card);
+});
